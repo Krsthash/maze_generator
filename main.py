de7a03af86d5d9628e676d28b691091b458541d6
Krsthash <87620691+Krsthash@users.noreply.github.com>
2022-12-02 15:59:25 +0100

    Canvas initialization.

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import tkinter as tk
 
 # Window initialization
+WIDTH = 500
+HEIGHT = 500
+
 root = tk.Tk()
 root.title('Maze Generator')
-root.geometry('500x500')
+root.geometry(f"{WIDTH}x{HEIGHT}")
 root.resizable(False, False)
+
+# Canvas initialization
+canvas = tk.Canvas(root, width=WIDTH, height=HEIGHT, bg='#8b9fc4', highlightthickness=0)
+canvas.pack()
 
 root.mainloop()
 
