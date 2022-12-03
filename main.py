06372b577f3313a2df02222bf6e606542f7258a7
Krsthash <87620691+Krsthash@users.noreply.github.com>
2022-12-03 11:41:02 +0100

    Added the distraction generation.

--- main.py
+++ main.py
@@ -10,54 +10,56 @@ cell_size = 50
 grid = []
 
 
+def is_valid(p, dir_):
+    # print(f"Pointer y: {p.y}\nPointer x: {p.x}\nDirection: {dir}\n\n")
+    if dir_ == 0:  # UP
+        if p.y != 0:
+            if grid[p.y // cell_size - 1][p.x // cell_size].visited:
+                return 0
+            else:
+                return 1  # Can visit the cell
+        else:
+            return 0
+    elif dir_ == 1:  # RIGHT
+        if p.x != width - cell_size:
+            if grid[p.y // cell_size][p.x // cell_size + 1].visited:
+                return 0
+            else:
+                return 1  # Can visit the cell
+        else:
+            return 0
+    elif dir_ == 2:  # DOWN
+        if p.y != height - cell_size:
+            if grid[p.y // cell_size + 1][p.x // cell_size].visited:
+                return 0
+            else:
+                return 1  # Can visit the cell
+        else:
+            return 0
+    elif dir_ == 3:  # LEFT
+        if p.x != 0:
+            if grid[p.y // cell_size][p.x // cell_size - 1].visited:
+                return 0
+            else:
+                return 1  # Can visit the cell
+        else:
+            return 0
+
+
+def get_choices(p):
+    choices = []
+    if is_valid(p, 0):
+        choices.append(0)
+    if is_valid(p, 1):
+        choices.append(1)
+    if is_valid(p, 2):
+        choices.append(2)
+    if is_valid(p, 3):
+        choices.append(3)
+    return choices
+
+
 def find_the_end(end_x, end_y):
-    def is_valid(p, dir):
-        # print(f"Pointer y: {p.y}\nPointer x: {p.x}\nDirection: {dir}\n\n")
-        if dir == 0:  # UP
-            if p.y != 0:
-                if grid[p.y // cell_size-1][p.x // cell_size].visited:
-                    return 0
-                else:
-                    return 1  # Can visit the cell
-            else:
-                return 0
-        elif dir == 1:  # RIGHT
-            if p.x != width-cell_size:
-                if grid[p.y // cell_size][p.x // cell_size+1].visited:
-                    return 0
-                else:
-                    return 1  # Can visit the cell
-            else:
-                return 0
-        elif dir == 2:  # DOWN
-            if p.y != height-cell_size:
-                if grid[p.y // cell_size+1][p.x // cell_size].visited:
-                    return 0
-                else:
-                    return 1  # Can visit the cell
-            else:
-                return 0
-        elif dir == 3:  # LEFT
-            if p.x != 0:
-                if grid[p.y // cell_size][p.x // cell_size-1].visited:
-                    return 0
-                else:
-                    return 1  # Can visit the cell
-            else:
-                return 0
-
-    def get_choices(p):
-        choices = []
-        if is_valid(p, 0):
-            choices.append(0)
-        if is_valid(p, 1):
-            choices.append(1)
-        if is_valid(p, 2):
-            choices.append(2)
-        if is_valid(p, 3):
-            choices.append(3)
-        return choices
-
     def backtracked_walls(p, last_p):
         p_y = p.y // cell_size
         p_x = p.x // cell_size
@@ -77,7 +79,7 @@ def find_the_end(end_x, end_y):
     pointer.visited = True
     while not (pointer.y == end_y-cell_size and pointer.x == end_x-cell_size):
         # print(pointer.y, pointer.x)
-        # sleep(1)
+        sleep(0.01)
         # Choose a random direction
         directions = get_choices(pointer)
         if len(directions):
@@ -110,8 +112,8 @@ def find_the_end(end_x, end_y):
                 pointer_array[-1].walls = [pointer_array[-1].walls[0], pointer_array[-1].walls[1], pointer_array[-1].walls[2], False]
                 pointer_array[-1].draw()
             pointer.visited = True
-            pointer_array[-1].highlight(2)
-            pointer.highlight(1)
+            # pointer_array[-1].highlight(2)
+            # pointer.highlight(1)
         else:
             pointer.highlight(0)
             pointer.walls = [True, True, True, True]
@@ -126,6 +128,69 @@ def find_the_end(end_x, end_y):
             pointer_array[-1].highlight(0)
             pointer_array.pop(-1)
     print("FOUND THE END!!")
+    return pointer_array
+
+
+def create_distractions(p_array):
+    n_cells_to_populate = (width//cell_size) * (height//cell_size)
+    for y in range(height//cell_size):
+        for x in range(width//cell_size):
+            grid[y][x].visited = False
+            if grid[y][x] in p_array:
+                n_cells_to_populate -= 1
+                grid[y][x].visited = True
+    print(n_cells_to_populate)
+    for path_cell in p_array:
+        d_array = []
+        pointer = path_cell
+        while True:
+            choices = get_choices(pointer)
+            print(choices, pointer.y//cell_size, pointer.x//cell_size)
+            if len(choices):
+                direction = choices[random.randint(0, len(choices)-1)]
+
+                pointer.visited = True
+                d_array.append(pointer)
+                n_cells_to_populate -= 1
+
+                if direction == 0:
+                    pointer = grid[pointer.y // cell_size - 1][pointer.x // cell_size]
+                    pointer.walls = [pointer.walls[0], pointer.walls[1], False, pointer.walls[3]]
+                    pointer.draw()
+                    d_array[-1].walls = [False, d_array[-1].walls[1], d_array[-1].walls[2], d_array[-1].walls[3]]
+                    d_array[-1].draw()
+                elif direction == 1:
+                    pointer = grid[pointer.y // cell_size][pointer.x // cell_size + 1]
+                    pointer.walls = [pointer.walls[0], pointer.walls[1], pointer.walls[2], False]
+                    pointer.draw()
+                    d_array[-1].walls = [d_array[-1].walls[0], False, d_array[-1].walls[2], d_array[-1].walls[3]]
+                    d_array[-1].draw()
+                elif direction == 2:
+                    pointer = grid[pointer.y // cell_size + 1][pointer.x // cell_size]
+                    pointer.walls = [False, pointer.walls[1], pointer.walls[2], pointer.walls[3]]
+                    pointer.draw()
+                    d_array[-1].walls = [d_array[-1].walls[0], d_array[-1].walls[1], False, d_array[-1].walls[3]]
+                    d_array[-1].draw()
+                elif direction == 3:
+                    pointer = grid[pointer.y // cell_size][pointer.x // cell_size - 1]
+                    pointer.walls = [pointer.walls[0], False, pointer.walls[2], pointer.walls[3]]
+                    pointer.draw()
+                    d_array[-1].walls = [d_array[-1].walls[0], d_array[-1].walls[1], d_array[-1].walls[2], False]
+                    d_array[-1].draw()
+                pointer.visited = True
+            else:
+                if pointer == path_cell:
+                    break
+                else:
+                    pointer = d_array[-1]
+                    d_array.pop(-1)
+    print(n_cells_to_populate)
+
+
+def generate_maze():
+    print("Generating the maze...")
+    arr = find_the_end(height, width)
+    create_distractions(arr)
 
 
 class Cell:
@@ -182,16 +247,16 @@ canvas = tk.Canvas(root, width=width, height=height, bg='#8b9fc4', highlightthic
 canvas.pack()
 
 # Populating the grid
-for y in range(height//cell_size):
+for i in range(height//cell_size):
     row = []
-    for x in range(width//cell_size):
-        cell = Cell(x * cell_size, y * cell_size)
+    for j in range(width//cell_size):
+        cell = Cell(j * cell_size, i * cell_size)
         row.append(cell)
         # Draw cells to the canvas
         cell.draw()
     grid.append(row)
 
-thread = threading.Thread(target=lambda: find_the_end(height, width))
+thread = threading.Thread(target=generate_maze)
 thread.start()
 
 root.mainloop()
